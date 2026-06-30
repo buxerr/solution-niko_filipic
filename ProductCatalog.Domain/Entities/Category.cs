c8039c3933bc6d687c4d6779b75ca0b2514d7f72
buxerr <nikotfic@gmail.com>
2026-06-30 18:18:05 +0200

    feat: add product and category domain models

---
+++ ProductCatalog.Domain/Entities/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductCatalog.Domain.Entities;
+
+public class Category
+{
+    public string Slug { get; set; } = string.Empty;
+
+    public string Name { get; set; } = string.Empty;
+}
