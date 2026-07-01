96da01e34d4425b00cfae6a18ea39e75d521a231
buxerr <nikotfic@gmail.com>
2026-07-01 17:31:14 +0200

    chore: switch to classic Swagger setup

---
+++ ProductCatalog.Infrastructure/DummyJson/Models/DummyJsonLoginResponseDto.cs
@@ -0,0 +1,22 @@
+﻿namespace ProductCatalog.Infrastructure.DummyJson.Models;
+
+internal class DummyJsonLoginResponseDto
+{
+    public int Id { get; set; }
+
+    public string Username { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string Gender { get; set; } = string.Empty;
+
+    public string Image { get; set; } = string.Empty;
+
+    public string AccessToken { get; set; } = string.Empty;
+
+    public string RefreshToken { get; set; } = string.Empty;
+}
